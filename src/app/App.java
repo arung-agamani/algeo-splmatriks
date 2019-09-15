ca7c03577054345220e94f4ae27ceac6eb3e5468
unknown <arung.agamani@gmail.com>
2019-09-15 20:09:18 +0700

    Test commit to see if my bot actually fucking works

--- src/app/App.java
+++ src/app/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
         System.out.println("This is a boilerplate project for Tubes Algeo 1. Have fun!");
-        System.out.println("And good luck.....");
+        System.out.println("And good luck...");
     }
 }
