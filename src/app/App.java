cacb5f4e2360fb76bee98afcfb209979c5c8a868
unknown <arung.agamani@gmail.com>
2019-09-16 03:13:48 +0700

    (Kinda) Finished Gauss-Jordan Elimination Algorithm on App.java

--- src/app/App.java
+++ src/app/App.java
@@ -14,7 +14,7 @@ public class App {
         mat = tr.ReadFileToMatrix("./src/components/data.txt");
         mat.tulisMatriks(md.row, md.col);
         //System.out.println("The data length is : \nRow : " + md.row + "\nCol : " + md.col);
-        System.out.println();
+        /* System.out.println();
         System.out.print("Row which the first element closest to 0 is : ");
         System.out.print(mat.SearchForClosestToZero(mat, md, 1, 1));
         System.out.println();
@@ -26,8 +26,85 @@ public class App {
         }
         System.out.println();
         mat.SwitchRows(mat, 2, 3);
-        mat.tulisMatriks(md.row, md.col);
+        mat.tulisMatriks(md.row, md.col); */
         
         // mat.AddOrSubstractRows(false, 1,2, mat);
+
+        // Algoritma Gauss-Jordan elimination
+        for (int i  = 0; i < md.row; i++) {
+                System.out.println();
+                mat.tulisMatriks(md.row, md.col);
+                if (mat.Mat[i][i] != 1) {
+                    mat.SwitchRows(mat, mat.SearchForClosestToZero(mat, md, i + 1, i+1), i + 1);
+                    System.out.println("switched + ");
+                    mat.tulisMatriks(md.row, md.col);
+                    System.out.println();
+                }
+                if (mat.Mat[i][i] != 1) {
+                    mat.ConstantMultRow(mat, i + 1 , mat.Mat[i][i], md);
+                    mat.tulisMatriks(md.row, md.col);
+                    System.out.println();
+                }
+                if (i == md.row - 1 && md.row == md.col - 1) {
+                    if (mat.Mat[i][i] == 1) {
+                        if (mat.Mat[i][i + 1] > 0) {
+                            System.out.println("Ada solusi!");
+                            break;
+                        } else if (mat.Mat[i][i + 1] == 0) {
+                            System.out.println("Solusi banyak!");
+                            break;
+                        } else {
+                            System.out.println("tidak ada solusi");
+                            break;
+                        }
+                    }
+                }
+                
+                for (int k = i + 1; k < md.row; k++) {
+                    while(mat.Mat[k][i] != 0) {
+                        if (mat.Mat[k][i] > 0) {
+                            mat.AddOrSubstractRows(false, i+1, k+1, mat);
+                        } else if (mat.Mat[k][i] < 0) {
+                            mat.AddOrSubstractRows(true, i+1, k+1, mat);
+                        }
+                        mat.tulisMatriks(md.row, md.col);
+                        System.out.println();
+                        
+                    }
+                    if (mat.CheckIfHasZeroRows(mat, md)) {
+                        System.out.println("Solusi banyak!");
+                        i = md.row;
+                        break;
+                    }
+                }
+                if (!mat.CheckIfHasZeroRows(mat, md)) {
+                    for (int k = i - 1; k >= 0; k--) {
+                        while(mat.Mat[k][i] != 0) {
+                            if (mat.Mat[k][i] > 0) {
+                                mat.AddOrSubstractRows(false, i+1, k+1, mat);
+                            } else if (mat.Mat[k][i] < 0) {
+                                mat.AddOrSubstractRows(true, i+1, k+1, mat);
+                            }
+                            mat.tulisMatriks(md.row, md.col);
+                            System.out.println();
+                        }
+                        if (mat.CheckIfHasZeroRows(mat, md)) {
+                            System.out.println("Solusi banyak!");
+                            i = md.row;
+                            break;
+                        }
+                    }
+                }
+                
+                if (i == md.row - 1 && md.row <= md.col - 1) {
+                    if (mat.Mat[i][i] == 1) {
+                            System.out.println("Solusi banyak!");
+                            i = md.row;
+                            break;
+                    }
+                }
+                
+            
+        }
     }
 }
--- src/components/matriks.java
+++ src/components/matriks.java
@@ -1,14 +1,15 @@
 package components;
 
 import java.lang.Math;
+import java.util.Arrays;
 
 public class Matriks {
     //atribut
-    public int[][] Mat;
+    public float[][] Mat;
     //method
 
     public Matriks(int a, int b) { //konstruktor:
-        this.Mat = new int[a][b];
+        this.Mat = new float[a][b];
         for (int i=0; i<a; i++) {
             for (int j=0; j<b; j++) {
             this.Mat[i][j] = 0;
@@ -39,9 +40,10 @@ public class Matriks {
         }
     }
 
-    public void transpose(int N, int M, int Matt[][]) {
+    public void transpose(int N, int M, float Matt[][]) {
         //membuat matriks transpose
-        int i, j, temp;
+        int i, j;
+        float temp;
 
         for (i=0; i<N; i++) {
             for (j=i; j<M; j++) {
@@ -52,28 +54,35 @@ public class Matriks {
         }
     }
 
+    public static float round2(float number, int scale) {
+        int pow = 10;
+        for (int i = 1; i < scale; i++)
+            pow *= 10;
+        float tmp = number * pow;
+        return ( (float) ( (int) ((tmp - (int) tmp) >= 0.5f ? tmp + 1 : tmp) ) ) / pow;
+    }
+
     public void AddOrSubstractRows(boolean isAdd, int sourceRow, int targetRow, Matriks targetMatriks) {
         // Belum jadi
         // to do : add/substract from source to target based on the boolean isAdd
-        int firstRow[] = targetMatriks.Mat[sourceRow-1];
-        int secondRow[] = targetMatriks.Mat[targetRow-1];
-        for (int i = 0; i < firstRow.length; i++) {
-            System.out.print(firstRow[i]);
-            System.out.print(" ");
+        float firstRow[] = targetMatriks.Mat[sourceRow-1];
+        int mult;
+        if (isAdd) {
+            mult = 1;
+        } else {
+            mult = -1;
         }
-        System.out.println();
-        for (int i = 0; i < secondRow.length; i++) {
-            System.out.print(secondRow[i]);
-            System.out.print(" ");
+        for (int i = 0; i < firstRow.length; i++) {
+            targetMatriks.Mat[targetRow-1][i] += targetMatriks.Mat[sourceRow-1][i]*mult;
         }
     }
 
     public int SearchForClosestToZero(Matriks targetMatriks, MatrixDimension md, int column, int startRow) {
-        int delta = 9999;
+        float delta = 9999;
         int src = 0;
         
         for (int i = startRow - 1; i < md.row; i++) {
-            if (Math.abs(targetMatriks.Mat[i][column-1]) >= delta) {
+            if (Math.abs(targetMatriks.Mat[i][column-1]) <= delta && targetMatriks.Mat[i][column-1] != 0) {
                 delta = Math.abs(targetMatriks.Mat[i][column-1]);
                 src = i;
             }
@@ -89,8 +98,33 @@ public class Matriks {
     }
 
     public void SwitchRows(Matriks targetMatriks, int sourceRow, int targetRow) {
-        int temp[] = targetMatriks.Mat[sourceRow-1];
+        float temp[] = targetMatriks.Mat[sourceRow-1];
         targetMatriks.Mat[sourceRow-1] = targetMatriks.Mat[targetRow-1];
         targetMatriks.Mat[targetRow-1] = temp;
+    }
+
+    public void ConstantMultRow(Matriks targetMatriks, int targetRow, float mult, MatrixDimension md) {
+        for (int i = 0; i < md.col; i++) {
+            targetMatriks.Mat[targetRow-1][i] = Math.round(targetMatriks.Mat[targetRow-1][i] * (1/mult)) ;
+        }
+    }
+
+    public Boolean CheckIfHasZeroRows(Matriks targetMatriks, MatrixDimension md) {
+        boolean isTrue = false;
+        for (int i = 0; i < md.row; i++) {
+            boolean rowClear;
+            rowClear = true;
+                for (int j = 0; j < md.col; j++) {
+                    if (targetMatriks.Mat[i][j] != 0) {
+                        rowClear = false;
+                        break;
+                    }
+                }
+            if (rowClear) {
+                isTrue = true;
+                break;
+            }
+        }
+        return isTrue;
     }
 }
