0b9e2a992385d00e6c77c1f650b51b7d724a48be
unknown <arung.agamani@gmail.com>
2019-09-15 22:29:59 +0700

    Lupa ngehapus main() function di TextReader.java lmao

--- src/components/TextReader.java
+++ src/components/TextReader.java
@@ -12,36 +12,6 @@ public class TextReader {
 
 
     // Implementation
-    public static void main(String[] args) throws Exception {
-        Matriks mat = new Matriks();
-        // KAMUS LOKAL //
-        String delimiter = " ";
-        String[] tempArray;
-        int iter;
-        Scanner sc;
-        File file;
-        file = new File("./src/components/data.txt");
-        sc = new Scanner(file);
-        iter = 0;
-        while(sc.hasNextLine()) {
-            String line = sc.nextLine();
-            tempArray = line.split(delimiter);
-            for (int i = 0; i < tempArray.length; i++) {
-                // System.out.print(tempArray[i]);
-                // System.out.print(" ");
-                mat.Mat[iter][i] = Integer.parseInt(tempArray[i]);
-            }
-            iter += 1;
-        }
-
-        mat.tulismatriks(10, 10);
-
-        System.out.println("Transposed matrix be like : ");
-        mat.transpose(10, 10, mat.Mat);
-        mat.tulismatriks(10, 10);
-
-        sc.close();
-    }
 
     public static Matriks ReadFileToMatrix(String path) throws FileNotFoundException {
         File file = new File(path);
