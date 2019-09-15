8414c4b8515736730125e04b05e1766256f17056
unknown <arung.agamani@gmail.com>
2019-09-15 16:18:24 +0700

    Initial commit

---
+++ src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+        System.out.println("This is a boilerplate project for Tubes Algeo 1. Have fun!");
+        System.out.println("And good luck.....");
+    }
+}
