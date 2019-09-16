e591afe4f6a69e611d9fb89943e3f4db09875c26
unknown <arung.agamani@gmail.com>
2019-09-16 08:40:48 +0700

    Cleaning up the output for Gauss-Jordan elimination. Note : Still not ended up having reduced echelon form on some cases.

--- src/app/App.java
+++ src/app/App.java
@@ -12,48 +12,36 @@ public class App {
         System.out.println("And good luck...");
         md = tr.CheckDataLength("./src/components/data.txt");
         mat = tr.ReadFileToMatrix("./src/components/data.txt");
+        System.out.println("Input matriks : ");
         mat.tulisMatriks(md.row, md.col);
-        //System.out.println("The data length is : \nRow : " + md.row + "\nCol : " + md.col);
-        /* System.out.println();
-        System.out.print("Row which the first element closest to 0 is : ");
-        System.out.print(mat.SearchForClosestToZero(mat, md, 1, 1));
-        System.out.println();
-        if (mat.Mat[mat.SearchForClosestToZero(mat, md, 1, 1)-1][0] < 0) {
-            mat.InvertRowSign(mat, md, 1);
-            mat.tulisMatriks(md.row, md.col);
-        } else {
-            mat.tulisMatriks(md.row, md.col);
-        }
-        System.out.println();
-        mat.SwitchRows(mat, 2, 3);
-        mat.tulisMatriks(md.row, md.col); */
-        
-        // mat.AddOrSubstractRows(false, 1,2, mat);
-
+        System.out.println("Output matriks setelah eleminasi Gauss-Jordan : ");
         // Algoritma Gauss-Jordan elimination
         for (int i  = 0; i < md.row; i++) {
-                System.out.println();
-                mat.tulisMatriks(md.row, md.col);
+                // System.out.println();
+                // mat.tulisMatriks(md.row, md.col);
                 if (mat.Mat[i][i] != 1) {
                     mat.SwitchRows(mat, mat.SearchForClosestToZero(mat, md, i + 1, i+1), i + 1);
-                    System.out.println("switched + ");
-                    mat.tulisMatriks(md.row, md.col);
-                    System.out.println();
+                    // System.out.println("switched + ");
+                    // mat.tulisMatriks(md.row, md.col);
+                    // System.out.println();
                 }
                 if (mat.Mat[i][i] != 1) {
                     mat.ConstantMultRow(mat, i + 1 , mat.Mat[i][i], md);
-                    mat.tulisMatriks(md.row, md.col);
-                    System.out.println();
+                    // mat.tulisMatriks(md.row, md.col);
+                    // System.out.println();
                 }
                 if (i == md.row - 1 && md.row == md.col - 1) {
                     if (mat.Mat[i][i] == 1) {
                         if (mat.Mat[i][i + 1] > 0) {
+                            mat.tulisMatriks(md.row, md.col);
                             System.out.println("Ada solusi!");
                             break;
                         } else if (mat.Mat[i][i + 1] == 0) {
+                            mat.tulisMatriks(md.row, md.col);
                             System.out.println("Solusi banyak!");
                             break;
                         } else {
+                            mat.tulisMatriks(md.row, md.col);
                             System.out.println("tidak ada solusi");
                             break;
                         }
@@ -67,11 +55,12 @@ public class App {
                         } else if (mat.Mat[k][i] < 0) {
                             mat.AddOrSubstractRows(true, i+1, k+1, mat);
                         }
-                        mat.tulisMatriks(md.row, md.col);
-                        System.out.println();
+                        // mat.tulisMatriks(md.row, md.col);
+                        // System.out.println();
                         
                     }
                     if (mat.CheckIfHasZeroRows(mat, md)) {
+                        mat.tulisMatriks(md.row, md.col);
                         System.out.println("Solusi banyak!");
                         i = md.row;
                         break;
@@ -85,10 +74,11 @@ public class App {
                             } else if (mat.Mat[k][i] < 0) {
                                 mat.AddOrSubstractRows(true, i+1, k+1, mat);
                             }
-                            mat.tulisMatriks(md.row, md.col);
-                            System.out.println();
+                            // mat.tulisMatriks(md.row, md.col);
+                            // System.out.println();
                         }
                         if (mat.CheckIfHasZeroRows(mat, md)) {
+                            mat.tulisMatriks(md.row, md.col);
                             System.out.println("Solusi banyak!");
                             i = md.row;
                             break;
@@ -98,6 +88,7 @@ public class App {
                 
                 if (i == md.row - 1 && md.row <= md.col - 1) {
                     if (mat.Mat[i][i] == 1) {
+                        mat.tulisMatriks(md.row, md.col);
                             System.out.println("Solusi banyak!");
                             i = md.row;
                             break;
