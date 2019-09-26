05ceaaa2857046abc20cfb19817f8e9b8357450a
Zaidan Naufal <zaidannaufal7@gmail.com>
2019-09-27 04:11:46 +0700

    Kramer bug solved
    yeah

--- src/components/Kramer.java
+++ src/components/Kramer.java
@@ -5,7 +5,7 @@ public class Kramer{
     public static void cramer(Matriks mat,Matriks nilai,int order){
         float detcram = 0;
         for (int i=0;i< order;i++ ){
-            detcram = MatriksKofaktor.DeterminanKofaktor(Cramerbagian(mat, order, i), order)/MatriksKofaktor.DeterminanKofaktor(mat, order);  
+            detcram = MatriksKofaktor.DeterminanKofaktor(Cramerbagian(mat,nilai, order, i), order)/MatriksKofaktor.DeterminanKofaktor(mat, order);  
             System.out.println("x"+(i + 1)+" = "+detcram);
         }
     }
@@ -14,11 +14,11 @@ public class Kramer{
         Matriks cram = new Matriks(order,order);
         for (int i=0;i<order;i++){
             for(int j=0;j<order;j++){
-                if (i==replacedcol){
-                    cram.Mat[replacedcol][j] = mat.Mat[replacedcol][j];
+                 if (j!=replacedcol){
+                    cram.Mat[i][j] = mat.Mat[i][j];
                 }
-                else {
-                    cram.Mat[i][j]= mat.Mat[i][j];
+                else{
+                    cram.Mat[i][replacedcol]= nilai.Mat[i][0];
                 }
             }
         }
